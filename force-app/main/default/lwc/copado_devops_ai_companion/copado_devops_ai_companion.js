93a33dd6e7d5431fde82a7c88421ffb8f71dc8ae
McGivneyC <89395825+McGivneyC@users.noreply.github.com>
2026-03-30 15:36:45 -0600

    Delete force-app/main/default directory

--- force-app/main/default/lwc/copado_devops_ai_companion/copado_devops_ai_companion.js
+++
@@ -1,8 +0,0 @@
-import { api } from 'lwc';
-import { LightningElement } from 'lwc';
-export default class Copado_devops_ai_companion extends LightningElement {
-    @api contextId;
-    @api max_tokens;
-    @api temperature;
-    @api engine;
-}
